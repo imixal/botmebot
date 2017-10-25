0efb39fea18a1920a18b6dd5e4192060768a13fe
Mixal <i.mixal@yandex.ru>
2017-10-25 11:37:56 +0400

    Added ninject and automapper

---
+++ Icogram/Icogram.Utility/AutoMapperModule.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Ninject.Modules;
+
+namespace Icogram.Utility
+{
+    public class AutoMapperModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IMapper>().ToMethod(AutoMapper).InSingletonScope();
+        }
+
+        public static IMapper AutoMapper(Ninject.Activation.IContext context)
+        {
+            Mapper.Initialize(config =>
+            {
+
+            });
+            return Mapper.Instance;
+        }
+    }
+}
---
+++ Icogram/Icogram.Utility/NinjectDependencyResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using AutoMapper;
+using Icogram.DbContext;
+using Icogram.Models.UserModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Ninject;
+using Ninject.Web.Common;
+
+namespace Icogram.Utility
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private static IKernel _kernel;
+
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            _kernel = kernelParam;
+            AddBindings();
+        }
+
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private static void AddBindings()
+        {
+            _kernel.Bind<IdentityDbContext<ApplicationUser>>().To<IcogramDbContext>().InRequestScope();
+            _kernel.Bind<IMapper>().ToMethod(AutoMapperModule.AutoMapper).InRequestScope();
+
+        }
+    }
+}
